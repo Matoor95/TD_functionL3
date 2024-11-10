82a0136410c75cc11a09fb8138e37fc9b9529b23
Matoor95 <seckmatar9521@gmail.com>
2024-11-10 10:20:37 +0100

    correction td

--- bin/td2.dart
+++ bin/td2.dart
@@ -1,4 +1,25 @@
-
 void main() {
-  
+  print(greet("Ali"));
+  var sum = calculateSum(12, 10);
+  print(sum);
+
+  print(isEven(4));
+
+  introduce(nom: "John Doe");
+}
+
+String greet(String nom) {
+  return "Bonjour , $nom";
+}
+
+int calculateSum(int a, int b) {
+  return a + b;
+}
+
+bool isEven(int a) {
+  return a % 2 == 0;
+}
+
+void introduce({required String nom, int age = 18}) {
+  print("Je m'appelle $nom et j'ai $age ans");
 }
